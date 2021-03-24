6ede73286d10bf96acc1433c4b89d217fdec597d
elis-py <efremova.elis@yandex.ru>
2021-03-24 15:17:07 +0300

    added main function

--- hangman_game.py
+++ hangman_game.py
@@ -6,6 +6,25 @@ Created on Thu Feb  4 15:20:25 2021
 
 import random
 
+def main():
+    global tries
+    print("***H A N G M A N***\n")
+    while tries != 0:
+        check_input()
+        if letter in random_word:
+            for k in range(len(random_word)):
+                if random_word[k] == letter:
+                    hidden_word[k] = letter
+            if "-" not in hidden_word:
+                print(f'You guessed the word {"".join(hidden_word)}!\nYou survived!')
+                break                 
+        else:
+            print("That letter doesn't appear in the word")
+            tries -= 1
+            attempts()
+        letters_input.add(letter)
+
+
 def check_input():
     global letter
     print("\n", "".join(hidden_word))
@@ -20,6 +39,9 @@ def check_input():
         print("Please enter a lowercase English letter")
         check_input()
 
+def attempts():
+    print(f'Remaining attempts: {tries}')
+
 
 words_choice = ('python', 'java', 'kotlin', 'javascript')
 random_word = random.choice(words_choice)
@@ -27,21 +49,7 @@ hidden_word = list(len(random_word) * "-")
 tries = 8
 letters_input = set()
 
-print("H A N G M A N\n")
-
-while tries != 0:
-    check_input()
-    if letter in random_word:
-        for k in range(len(random_word)):
-            if random_word[k] == letter:
-                hidden_word[k] = letter
-        if "-" not in hidden_word:
-            print(f'You guessed the word {"".join(hidden_word)}!\nYou survived!')
-            break                 
-    else:
-        print("That letter doesn't appear in the word")
-        tries -= 1
-    letters_input.add(letter)
+main()
      
 if "-" in hidden_word:
     print('You lost!')
